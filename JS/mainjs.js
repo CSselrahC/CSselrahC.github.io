30c3bbef646ccf7f228e81dceee7c7aa162489fe
Charles C <110767536+CSselrahC@users.noreply.github.com>
2023-11-13 15:52:05 +0800

    Update mainjs.js
    age counter

--- JS/mainjs.js
+++ JS/mainjs.js
@@ -116,6 +116,20 @@
 		}
 	} 
 	
+	/* age */
+	
+	var currentYear = new Date().getFullYear();
+	var currentMonth = new Date().getMonth() + 1;
+	var currentDate = new Date().getDate();
+	
+	var age = currentYear - 2004;
+	
+	if (currentMonth < 9 || (currentMonth == 9 && currentDate < 6)) {
+        	age = age - 1;
+    	}
+	
+	document.getElementById("myAge").innerHTML = age;
+	
 	/* intro */
 	let introToggle = document.getElementById("aboutMe");
 	let ykIntro = document.getElementById("intro");
@@ -127,7 +141,7 @@
 		introID.innerHTML == 'noIntro';
 		if (introID.innerHTML == 'noIntro' ) {
 			introID.innerHTML = 'withIntro'
-			ykIntro.innerHTML = "<p> My name is Charles Carreon. I'm 17 years old. My house is in Laguna, where all the villas are, and I am not married. I currently don't work, and I am at home almost every day. I don't smoke, and I don't drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone. </p>"
+			ykIntro.innerHTML = "<p> My name is Charles Carreon. I'm " + age + " years old. My house is in Laguna, where all the villas are, and I am not married. I currently don't work, and I am at home almost every day. I don't smoke, and I don't drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone. </p>"
 		}
 
 		else if (introID != 'noIntro' ){
